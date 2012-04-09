d86f0ad63cd88ed48113e5ca9b25ac7914029c88
Brad Forbes <bradbforbes@gmail.com>
2012-04-09 13:22:14 -0400

    Documented our GocSlideManager constructor.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,42 +1,67 @@
+/**
+ * Initialize the slide manager when page finishes loading.
+ */
 jQuery(document).ready(function($){
-
-
     var manager = new GocSlideManager($);
-
 });
 
+
+/**
+ * A class (constructor function) to manage our slides.
+ *
+ * Remembers which slides the user has clicked, and saves
+ * them to the hidden field 'manage-slides-selected'.
+ *
+ * Adds and removes 'is-selected' classes to slides
+ * to provide a visual cue.
+ *
+ * @param $ jQuery object
+ */
 function GocSlideManager($) {
 
-    console.log('Loaded');
-
     /**
-     * An array to hold our selected attachment IDs.
+     * A single-dimensional array to hold our selected attachment ID integers.
+     *
+     * @param {array}
      */
     var selected = [];
 
     // Add initial selected IDs (that were previously saved)
     // to selected collection.
     var selectedIds = $('#manage-slides-selected').val();
+
+    // Confirm that there are some selected slides (sometimes there won't be).
     if (selectedIds) {
+
+        // Selected slides come in a string of comma-separated items,
+        // so split them into pieces to make our job easier.
         var selectedIds = selectedIds.split(',');
+
+        // Add each slide ID to our array of selected slide IDs.
         for (var selectedId in selectedIds) {
             var id = parseInt(selectedIds[selectedId]);
             selected.push(id);
         }
-        console.log('Selected:' + selected);
     }
 
+    /**
+     * Saves the selected slides to our hidden field.
+     *
+     * Slides are saved every time a slide is clicked.
+     */
     var save = function save() {
 
-        console.log('Selected: ' + selected);
-
+        // JS will automatically translate our array
+        // to a string of comma-separated items.
         $('#manage-slides-selected').val(selected);
-
     }
 
+    /**
+     * Selects or de-selects a slide whenever one is clicked,
+     * and adds/removes CSS 'is-selected' class, which provides
+     * the visual cue for individual, selected slides.
+     */
     $('.goc-slide').click(function(){
-
-        console.log('Clicked');
 
         // Get full ID of element.
         var elementId = $(this).attr('id');
@@ -48,18 +73,18 @@ function GocSlideManager($) {
         // selected collection.
         var foundIndex = $.inArray(selectedId, selected);
 
-        // Add the ID if it is not in the collection yet.
+        // Check if the slide is in our array of selections.
         if (foundIndex === -1) {
+            // Slide wasn't selected, select it.
             selected.push(selectedId);
             $(this).addClass('is-selected');
-            console.log($(this).attr('class'));
-
         } else {
+            // Slide was selected, deselect it.
             selected.splice(foundIndex, 1);
             $(this).removeClass('is-selected');
-            console.log($(this).attr('class'));
         }
 
+        // Save latest selected slides to our hidden field.
         save();
 
     });
